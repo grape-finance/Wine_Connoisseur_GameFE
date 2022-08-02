2a5cd76cbf613f99385904f1d08e85c30d0d96c6
grapefi <96745303+grapefi@users.noreply.github.com>
2022-08-03 07:16:55 +1000

    skills

--- src/config/skills.ts
+++ src/config/skills.ts
@@ -195,25 +195,25 @@ export const skills: ISkills = {
     {
       name: "Farm Hands",
       skillPoint: 1,
-      definition: "Increase Vintage store 800",
+      definition: "Increase Vintage store 1500",
       image: farm_hands,
     },
     {
       name: "Storage Shed",
       skillPoint: 2,
-      definition: "Increase Vintage store 1200",
+      definition: "Increase Vintage store 3000",
       image: storage_shed,
     },
     {
       name: "Hardy Varaities",
       skillPoint: 3,
-      definition: "Increase Vintage store 2500",
+      definition: "Increase Vintage store 10000",
       image: hardy_varaities,
     },
     {
       name: "Vine Mastery",
       skillPoint: 4,
-      definition: "Increase Vintage store 5000",
+      definition: "Increase Vintage store 30000",
       image: vine_mastery,
     },
     {
